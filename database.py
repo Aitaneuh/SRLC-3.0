63c0940df7d10431769e807ec1235c55e5dac6d2
Aitaneuh <ethanb9407@gmail.com>
2024-10-17 16:46:45 +0200

    !set-rank done

--- database.py
+++ database.py
@@ -6,3 +6,21 @@ async def create_tables():
         cursor = await db.execute("CREATE TABLE IF NOT EXISTS matchs (match_id INTEGER AUTO_INCREMENT PRIMARY KEY, host_id TEXT, rank TEXT, status TEXT, FOREIGN KEY (host_id) REFERENCES users(discord_id))")
         await cursor.close()
         await db.commit()
+
+async def create_user(discord_id, elo, rank):
+    async with aiosqlite.connect('Main.db') as db:
+        cursor = await db.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)", (discord_id, elo, rank, 0, 0, "inactive"))
+        await cursor.close()
+        await db.commit()
+
+async def update_user(discord_id, elo, rank):
+    async with aiosqlite.connect('Main.db') as db:
+        cursor = await db.execute("UPDATE users SET elo = ?, rank = ? WHERE discord_id = ?", (elo, rank, discord_id))
+        await cursor.close()
+        await db.commit()
+
+async def user_exist(discord_id):
+    async with aiosqlite.connect('Main.db') as db:
+         async with db.execute("SELECT * FROM users WHERE discord_id = ?", (discord_id,)) as cursor:
+            user = await cursor.fetchone()
+            return user
--- main.py
+++ main.py
@@ -23,17 +23,17 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), activity=a
 
 @bot.event
 async def on_ready():
-    # Connexion à la base de données
+
     bot.db = await aiosqlite.connect('Main.db')
 
-    # Création de la table si elle n'existe pas
+
     await create_tables()
 
     print("Bot is online ! ", "| Name :", bot.user.name, "| ID :", bot.user.id)
     print("//////////////////////////////////")
     try:
         synced = await bot.tree.sync()
-        synced_names = [command.name for command in synced]  # Récupère les noms des commandes synchronisées
+        synced_names = [command.name for command in synced]  
         print(f"{len(synced)} commands have been synced : {', '.join(synced_names)}")
     except Exception as e:
         print(e)
@@ -42,17 +42,77 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    # Récupérer le serveur (guild) où le membre a rejoint
+
     guild = member.guild
     
-    # Récupérer le rôle que vous souhaitez attribuer au membre
+
     role = discord.utils.get(guild.roles, id=1296237890329776129)
 
-    # Vérifier si le rôle existe et si le membre n'a pas déjà ce rôle
     if role is not None and role not in member.roles:
-        # Ajouter le rôle au membre
+
         await member.add_roles(role)
 
 #-------------------------------------------------------------------------------------------------------------------------
+
+@bot.command(name="set-rank")
+async def set_rank(ctx, member: discord.Member, rank: str):
+
+    if not ctx.author.guild_permissions.administrator:
+        await ctx.send("You are not an admin, so you can't use this command.")
+        return
+
+    if member is None or rank is None:
+        await ctx.send("Usage : `!set-rank @User Rank`")
+        return
+    
+    guild = ctx.guild
+    if guild.get_member(member.id) is None:
+        await ctx.send(f"The user {member.mention} is not found in this server.")
+        return
+    
+    if rank not in ["S", "X", "A", "B"]:
+        await ctx.send("Rank is not correct, possible ranks are : **S**, **X**, **A**, **B**")
+        return
+    
+    if rank == "S":
+        elo = 2000
+    elif rank == "X":
+        elo = 1600
+    elif rank == "A":
+        elo = 1200
+    elif rank == "B":
+        elo = 800
+
+    discord_id = str(member.id)
+
+    role_name = rank
+    role = discord.utils.get(guild.roles, name=role_name)
+    player_role = discord.utils.get(guild.roles, id=1296463921783509036)
+
+    if role is not None:
+        rank_roles = ["S", "X", "A", "B"]
+        member_roles = [r for r in member.roles if r.name in rank_roles]
+        await member.remove_roles(*member_roles)
+
+        await member.add_roles(role)
+        await member.add_roles(player_role)
+    else:
+        await ctx.send(f"The role '{role_name}' does not exist in this server.")
+
+
+    user = await user_exist(discord_id)
+
+    if user is not None:
+        await update_user(discord_id, elo, rank)
+        await ctx.send(f"Rank of {member.mention} was updated to **{rank}** with an elo of **{elo}**.")
+    else:
+        await create_user(discord_id, elo, rank)
+        await ctx.send(f"Rank of {member.mention} was set to **{rank}** with an elo of **{elo}**.")
+
+    
+
+    
+#-------------------------------------------------------------------------------------------------------------------------
+
 
 bot.run(TOKEN)
